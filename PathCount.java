d2458d4eeef54102a0d45b1288f45135a2515d30
goodie <vorkuta@inbox.ru>
2023-02-13 21:33:44 +0300

    Initial commit

---
+++ PathCount.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class PathCount {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("PathCount");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setUndecorated(true);
+        frame.setVisible(true);
+    }
+    
+}
